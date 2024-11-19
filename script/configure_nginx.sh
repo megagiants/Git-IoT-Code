b330693c9d3896ad5393cc40a4d364c86b523772
megagiants <eajluoy@gmail.com>
2024-11-19 11:08:16 +0900

    Delete script directory

--- script/configure_nginx.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Nginx 설정 파일의 포트를 81로 변경
-sudo sed -i 's/listen 80;/listen 81;/' /etc/nginx/nginx.conf
-# server_names_hash_bucket_size 128; 추가
-sudo sed -i '/http {/a\    server_names_hash_bucket_size 128;' /etc/nginx/nginx.conf
-# Nginx 서비스 재시작
-sudo systemctl restart nginx
--- script/start_server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Gunicorn 서비스 파일 복사
-sudo cp /home/ec2-user/app/gunicorn.service /etc/systemd/system/gunicorn.service
-
-# 시스템 데몬 리로드 및 Gunicorn 서비스 시작
-sudo systemctl daemon-reload
-sudo systemctl enable gunicorn
-sudo systemctl restart gunicorn
